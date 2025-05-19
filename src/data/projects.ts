9fa14b1a9ea245273f7d804cc0e927180671f0d1
Elvis pino <elvisreyxd@gmail.com>
2025-05-19 19:31:57 -0400

    v1

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -23,7 +23,7 @@ export const projects: Project[] = [
       { name: "Vercel Blob", color: "#000000" },
       { name: "Tailwind CSS", color: "#06B6D4" },
     ],
-    liveLink: "https://my-porfolio-next-v1.vercel.app/",
+    liveLink: "https://my-ecommerce-k0zn44dvp-elvisxds-projects.vercel.app/",
     codeLink: "#",
     status: "Completed",
     featured: true,
